2d300764e4ce72eb4ff01f0d317b57c6da8146a1
AnDrEwHaLiEm <an.roooof@gmail.com>
2022-10-18 16:16:54 +0200

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import taskRouter from './Router/taskRouter';
-
+import cors from 'cors';
 dotenv.config();
-
 const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/task', taskRouter);
 
